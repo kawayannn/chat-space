fb2c87b76d2ef5a74113eb71133c5441d268ec41
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-10-16 18:36:59 +0900

    drop-migtare

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :users_groups
-  has_many :groups,         through: :users_groups
-  has_many :messages
-end
--- db/migrate/20191016032446_create_users_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users_groups do |t|
-      t.integer    :group_id,      null: false
-      t.integer    :user_id,       null: false
-      t.timestamps
-    end
-  end
-end
