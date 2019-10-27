365c7cc3b0092d674af1f6c508501ca283b70e53
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-10-27 17:37:49 +0900

    table_rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root to: 'messages#index'
+=======
+  devise_for :users
+  root to: 'groups#index'
+  resources :users,     only: [:edit, :update]
+  resources :groups,    only: [:new, :edit, :create, :update]
+  resources :messages,  only: [:index, :create]
+  # resources :groups do
+  # resources :messages 
+  # end
+>>>>>>> Stashed changes
 end
