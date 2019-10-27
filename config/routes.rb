399305f1a7bd93e9bb41a4f1dff6ce1513bb1e70
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-10-27 19:59:03 +0900

    CarrieWave

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
   resources :users,  only: [:edit, :update]
+<<<<<<< Updated upstream
   resources :groups, only: [:new, :edit, :create, :update]
+=======
+  resources :groups, only: [:new, :edit, :create, :update] do
+    resources :messages, only: [:index, :create]
+  end
+  validates :content, presence: true, unless: :image?
+
+>>>>>>> Stashed changes
 end
