c9c9f2d2f76a55122199ea74019fe6d17959a305
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-10-29 01:47:05 +0900

    validate_text_image

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :content_or_image, presence: true
+
+  private
+    def content_or_image
+      content.presence or image.presence
+    end
 end
