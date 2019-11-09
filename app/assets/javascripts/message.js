732adbcf9bf7d9879d7d51d2c94f991dbe10deed
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-11-09 19:23:24 +0900

    message.js_add_turbbolinks

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,60 @@
-$(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
+$(document).on('turbolinks:load', function() {
 
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+  $(function(){
+
+    function new_message(message){
+      
+      var imageHTML = message.image  ? `<img class src="${message.image}" >`: '';
+
+      var new_message = `<div class="messages__message">
+                          <div class="uper-message">
+                            <div class="uper-message__name">
+                              ${message.name}
+                            </div>
+                            <div class="uper-message__date">
+                              ${message.created_at}
+                            </div>
+                          </div>
+                          <div class="lower-message">
+                            <p class="lower-message__content">
+                              ${message.content}
+                            </p>
+                            <div class="lower-message__image">
+                              ${imageHTML}
+                            </div>
+                          </div>
+                        </div>`
+      return new_message;
+    }
+
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      
+      .done(function(message){
+        
+        var html = new_message(message);
+
+        $('.messages').append(html);
+        $('.inputbox__message').val('');
+        $('.mask__label--image').val('');
+        $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+        $('.sendmessage').prop('disabled', false);
+      })
+
+      .fail(function(message){
+        alert('エラー');
+      });
     })
-  })
+  });
 });
-
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,9 +1,7 @@
-json.array! @messages do |message|
-  json.id message.id
-  json.content message.content
-  json.image message.image
-  json.group_id message.group_id
-  json.user_id message.user_id
-  json.created_at message.created_at
-  json.updated_at message.updated_at
-end
+json.id @message.id
+json.name @message.user.name
+json.content @message.content
+json.image @message.image.url
+json.group_id @message.group_id
+json.user_id @message.user_id
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
