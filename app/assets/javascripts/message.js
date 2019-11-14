f3f5e396d232de611a16c6b9beacf4cde97cd1e8
kawayannn <56496098+kawayannn@users.noreply.github.com>
2019-11-14 20:06:39 +0900

    add,dlete_btn  turbolinks_delete

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,58 +1,56 @@
-$(document).on('turbolinks:load', function() {
+$(function(){
 
-  $(function(){
+  function new_message(message){
+    
+    var imageHTML = message.image  ? `<img class src="${message.image}" >`: '';
 
-    function new_message(message){
-      
-      var imageHTML = message.image  ? `<img class src="${message.image}" >`: '';
-
-      var new_message = `<div class="messages__message">
-                          <div class="uper-message">
-                            <div class="uper-message__name">
-                              ${message.name}
-                            </div>
-                            <div class="uper-message__date">
-                              ${message.created_at}
-                            </div>
+    var new_message = `<div class="messages__message">
+                        <div class="uper-message">
+                          <div class="uper-message__name">
+                            ${message.name}
                           </div>
-                          <div class="lower-message">
-                            <p class="lower-message__content">
-                              ${message.content}
-                            </p>
-                            <div class="lower-message__image">
-                              ${imageHTML}
-                            </div>
+                          <div class="uper-message__date">
+                            ${message.created_at}
                           </div>
-                        </div>`
-      return new_message;
-    }
+                        </div>
+                        <div class="lower-message">
+                          <p class="lower-message__content">
+                            ${message.content}
+                          </p>
+                          <div class="lower-message__image">
+                            ${imageHTML}
+                          </div>
+                        </div>
+                      </div>`
+    return new_message;
+  }
 
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
 
-      $.ajax({
-        url: url,
-        type: 'POST',
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      
-      .done(function(message){
-        var html = new_message(message);
-
-        $('.messages').append(html);
-        $("#new_message")[0].reset();
-        $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
-        $('.sendmessage').prop('disabled', false);
-      })
-
-      .fail(function(message){
-        alert('エラー');
-      });
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
-  });
+    
+    .done(function(message){
+      var html = new_message(message);
+
+      $('.messages').append(html);
+      $("#new_message")[0].reset();
+      $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+      $('.sendmessage').prop('disabled', false);
+    })
+
+    .fail(function(message){
+      alert('エラー');
+    });
+  })
 });
+
